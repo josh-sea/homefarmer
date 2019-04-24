547d87c02dceb9404863052c753b666ae47244aa
josh-sea <joshua.cocciardi@gmail.com>
2019-04-24 14:03:06 -0400

    still catching up to master

--- src/components/PlantTable.js
+++ src/components/PlantTable.js
@@ -1,59 +1,54 @@
 import React from 'react';
-import { Header, Table, Rating } from 'semantic-ui-react'
+import { Header, Table } from 'semantic-ui-react'
+import { connect } from 'react-redux'
 
-const  PlantTable = ({}) => (
+const  PlantTable = ({plantData}) => (
   <Table celled padded>
       <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell singleLine>Evidence Rating</Table.HeaderCell>
-          <Table.HeaderCell>Effect</Table.HeaderCell>
-          <Table.HeaderCell>Efficacy</Table.HeaderCell>
-          <Table.HeaderCell>Consensus</Table.HeaderCell>
-          <Table.HeaderCell>Comments</Table.HeaderCell>
+        <Table.Row style={{textAlign: 'center'}}>
+          <Table.HeaderCell singleLine>Plant Name</Table.HeaderCell>
+          <Table.HeaderCell singleLine>Avg. pH</Table.HeaderCell>
+          <Table.HeaderCell singleLine>Avg. EC mS</Table.HeaderCell>
+          <Table.HeaderCell singleLine>Plant Notes</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
 
-      <Table.Body>
-        <Table.Row>
-          <Table.Cell>
-            <Header as='h2' textAlign='center'>
-              A
-            </Header>
-          </Table.Cell>
-          <Table.Cell singleLine>Power Output</Table.Cell>
-          <Table.Cell>
-            <Rating icon='star' defaultRating={3} maxRating={3} />
-          </Table.Cell>
-          <Table.Cell textAlign='right'>
-            80% <br />
-            <a href='#'>18 studies</a>
-          </Table.Cell>
-          <Table.Cell>
-            Creatine supplementation is the reference compound for increasing muscular creatine
-            levels; there is variability in this increase, however, with some nonresponders.
-          </Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>
-            <Header as='h2' textAlign='center'>
-              A
-            </Header>
-          </Table.Cell>
-          <Table.Cell singleLine>Weight</Table.Cell>
-          <Table.Cell>
-            <Rating icon='star' defaultRating={3} maxRating={3} />
-          </Table.Cell>
-          <Table.Cell textAlign='right'>
-            100% <br />
-            <a href='#'>65 studies</a>
-          </Table.Cell>
-          <Table.Cell>
-            Creatine is the reference compound for power improvement, with numbers from one
-            meta-analysis to assess potency
-          </Table.Cell>
-        </Table.Row>
-      </Table.Body>
+      {
+        plantData.map(plant=>{
+        return <Table.Body>
+          <Table.Row>
+            <Table.Cell>
+              <Header as='h3' singleLine textAlign='center'>
+                {plant.name}
+              </Header>
+            </Table.Cell>
+            <Table.Cell singleLine textAlign='center'>
+              {plant.pH}
+            </Table.Cell>
+            <Table.Cell singleLine textAlign='center'>
+              {plant.ec}
+            </Table.Cell>
+            <Table.Cell>
+              {plant.plant_notes}
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      })
+    }
     </Table>
   );
 
-export default PlantTable;
+  const mapStateToProps = (state) => {
+    return {
+      plantData: state.plantData,
+    }
+  }//end of state to props
+  //###############################################################
+
+  //###############################################################
+  //dispatch as props from redux store
+  const mapDispatchToProps = {
+  }//end of dispatch to props
+  //###############################################################
+
+  export default connect(mapStateToProps, mapDispatchToProps)(PlantTable);
