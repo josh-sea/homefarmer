3936cae818632697201f1f9c008ac63dcd2acadf
josh-sea <joshua.cocciardi@gmail.com>
2019-04-24 15:22:21 -0400

    added name search for plants, ready for heroku

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Home from './containers/Home'
 import Community from './containers/Community'
 import Code from './containers/Code'
 import Data from './containers/Data'
-// const BASEURL = 'https://homefarmer-back.herokuapp.com/'
-const BASEURL = 'http://localhost:9000/'
+const BASEURL = 'https://homefarmer-back.herokuapp.com/'
+// const BASEURL = 'http://localhost:9000/'
 
 class App extends Component {
 
