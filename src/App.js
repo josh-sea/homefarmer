cb2408c576f55200a60d0380fbd6bec054d84a20
josh-sea <joshua.cocciardi@gmail.com>
2019-04-24 14:02:34 -0400

    local catching up to master

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ import Home from './containers/Home'
 import Community from './containers/Community'
 import Code from './containers/Code'
 import Data from './containers/Data'
+const BASEURL = 'https://homefarmer-back.herokuapp.com/'
+// const BASEURL = 'http://localhost:9000/'
 
 class App extends Component {
 
+  componentDidMount() {
+    fetch(`${BASEURL}api/v1/plants`)
+    .then(r=>r.json())
+    .then(plants=>{
+      this.props.getPlants(plants)
+    })
+  }
 
   render() {
 
